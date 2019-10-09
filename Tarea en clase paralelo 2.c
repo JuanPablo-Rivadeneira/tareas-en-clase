6e5ef65f07ac059d27f8553eacd5a8c858e97a62
JuanPablo-Rivadeneira <55768624+JuanPablo-Rivadeneira@users.noreply.github.com>
2019-10-08 21:37:16 -0500

    Add files via upload

---
+++ Tarea en clase paralelo 2.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+2. Realizar un programa en C que imprima Hola Mundo usando asteriscos.                           //problema asigando
+
+*******************************************************************************/
+#include <stdio.h>                                                                               //libreria
+
+int main()                                                                                       //funcion principal
+{
+    printf("\n");                                                                                //salto de linea
+    printf("\n");                                                                                //salto de linea
+    printf("      ***  ***   *********  ***         ***      \n");                               //imprimir en pantalla
+    printf("      ***  ***   *********  ***       **   **    \n");                               //imprimir en pantalla
+    printf("      ***  ***   **     **  ***      ***   ***   \n");                               //imprimir en pantalla
+    printf("      ********   **     **  ***     ****   ****  \n");                               //imprimir en pantalla
+    printf("      ********   **     **  ***     ************ \n");                               //imprimir en pantalla
+    printf("      ***  ***   *********  ******* ************ \n");                               //imprimir en pantalla
+    printf("      ***  ***   *********  ******* *****   **** \n");                               //imprimir en pantalla                           
+    printf("\n");                                                                                //salto de linea
+    printf("\n");                                                                                //salto de linea
+    
+    printf("      *****     *****     ***     ***   ******   ***   ********   *********  \n");   //imprimir en pantalla
+    printf("      *****     *****     ***     ***   ******   ***   ***   ***  *********  \n");   //imprimir en pantalla
+    printf("     ****  *   *  ****    ***     ***   *** ***  ***   ***   ***  **     **  \n");   //imprimir en pantalla
+    printf("    ****   *****   ****   ***     ***   ***  *** ***   ***   ***  **     **  \n");   //imprimir en pantalla
+    printf("   ****             ****  ***********   ***   ******   ***   ***  *********  \n");   //imprimir en pantalla
+    printf("  *****             ***** ***********   ***    *****   ********   *********  \n");   //imprimir en pantalla
+
+    return 0;
+}
